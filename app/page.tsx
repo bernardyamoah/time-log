063f913dbc3790a0eed14b2c35b3ffbbc8a84d35
bernardyamoah <bernardyamoah2016@gmail.com>
2024-01-24 11:21:49 +0000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,17 @@ import CalendarView from "@/components/calendar-view";
 import Logs from "@/components/logs";
 import Navbar from "@/components/navbar";
 import { NewLog } from "@/components/new-log";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+	const supabase = createServerComponentClient({ cookies });
+	const { data } = await supabase.auth.getSession();
+	console.log("🚀 ~ Home ~  data :", data);
+	if (!data.session) {
+		return redirect("/auth");
+	}
 	return (
 		<main className="p-5 space-y-10">
 			<Navbar />
